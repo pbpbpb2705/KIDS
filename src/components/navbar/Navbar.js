2c79647afb76ed4757f0e9268eac6f93bfbbc631
Pham Bảo <phambao2705@gmail.com>
2021-06-26 11:39:52 +0700

    commit #2

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import "./Navbar.css";
 function Navbar() {
     return (
-      <div>
-        <Link to="/" class="navbar-logo">Kids</Link>
-        <ul class="navbar-tab">
-          <Link to="/past" class="navbar-item">Past events</Link>
-          <Link to="/upcoming" class="navbar-item">Upcoming events</Link>
-          <Link to="/apply" class="navbar-item">Apply now!</Link>
-        </ul>
+      <div className="navbar-container">
+        <Link to="/" className="navbar-item navbar-logo icon">
+          Kids
+        </Link>
+        <nav className="navbar-tab">
+          <Link to="/past" className="navbar-item navbar-category">
+            Past events
+          </Link>
+          <Link to="/upcoming" className="navbar-item navbar-category">
+            Upcoming events
+          </Link>
+          <Link to="/apply" className="navbar-item navbar-category">
+            Apply now!
+          </Link>
+        </nav>
       </div>
     );
 }

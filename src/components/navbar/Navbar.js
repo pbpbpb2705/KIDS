7ad8b877a22d69619dc9026745e36a1af750ab90
Pham Bảo <phambao2705@gmail.com>
2021-07-31 23:52:40 +0700

    responsive #2

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -27,13 +27,34 @@ function Navbar() {
 
         <div className={classes.menu}>
           <nav>
-            <ul>
+            <ul className={classes.mainNav}>
               <li className={classes.button}>
                 <Link to="/about"> About </Link>
               </li>
 
-              <li className={classes.button}>
-                <Link to="/pastprojects"> Past Projects </Link>
+              <li>
+                <li className={classes.dropdown}>
+                  <li className={classes.button}>
+                    <Link to="/pastprojects"> Past Projects </Link>
+                  </li>
+
+                  <div className={classes.dropcontent}>
+                    <ul>
+                      <li>
+                        <Link to="/2020-2021"> 2020 - 2021 </Link>
+                      </li>
+                      <li>
+                        <Link to="/2018-2019"> 2018 - 2019 </Link>
+                      </li>
+                      <li>
+                        <Link to="/2016-2017"> 2016 - 2017 </Link>
+                      </li>
+                      <li>
+                        <Link to="/2014-2015"> 2014 - 2015 </Link>
+                      </li>
+                    </ul>
+                  </div>
+                </li>
               </li>
 
               <li className={classes.button}>
@@ -46,40 +67,42 @@ function Navbar() {
             </ul>
           </nav>
 
-          <form className={classes.searchform}>
-            <input
-              type="search"
-              name="q"
-              id="searchbar"
-              placeholder="Search"
-              required
-            />
-            <img
-              src="https://cdn.shopify.com/s/files/1/1170/9506/t/74/assets/icon-search.svg?v=3653875556752188748"
-              alt="search icon"
-              onClick={handleSearch}
-            />
-          </form>
           <div className={classes.container}>
-            <img className={classes.icon} src="Icon.svg" />
-            <div className={classes.dropdown}>
-              <ul>
-                <li className={classes.button}>
-                  <Link to="/about"> About </Link>
-                </li>
+            <form className={classes.searchform}>
+              <input
+                type="search"
+                name="q"
+                id="searchbar"
+                placeholder="Search"
+                required
+              />
+              <img
+                src="https://cdn.shopify.com/s/files/1/1170/9506/t/74/assets/icon-search.svg?v=3653875556752188748"
+                alt="search icon"
+                onClick={handleSearch}
+              />
+            </form>
+            <div className={classes.dropdown_container}>
+              <img className={classes.icon} src="Icon.svg" alt="list" />
+              <div className={classes.dropdown_responsive}>
+                <ul>
+                  <li className={classes.button_responsive}>
+                    <Link to="/about"> About </Link>
+                  </li>
 
-                <li className={classes.button}>
-                  <Link to="/pastprojects"> Past Projects </Link>
-                </li>
+                  <li className={classes.button_responsive}>
+                    <Link to="/pastprojects"> Past Projects </Link>
+                  </li>
 
-                <li className={classes.button}>
-                  <Link to="/contact"> Contact </Link>
-                </li>
+                  <li className={classes.button_responsive}>
+                    <Link to="/contact"> Contact </Link>
+                  </li>
 
-                <li className={classes.button}>
-                  <Link to="/upcoming"> Upcoming </Link>
-                </li>
-              </ul>
+                  <li className={classes.button_responsive}>
+                    <Link to="/upcoming"> Upcoming </Link>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
